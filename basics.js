1e97dbf36b58b3a753c09a762c122437fa170b35
vigo28GM <gamingvigo06@gmail.com>
2024-11-25 09:19:29 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,30 @@
+let isStudent = true
+
+if (person = isStudent){
+    console.log("Students!");
+} else {
+    console.log("Nav students!");
+}
+let numArray = [17,3,54];
+for (i = 0; i < numArray.length; i++){
+    console.log(numArray[i]);
+}
+
+
+
+function printErrorStatus() {
+
+    let Text = Bad_Gateway;
+    let code = 502;
+
+    if (Text == Bad_Gateway){
+        console.log("Lapa ir atrasta");
+    } else {
+        console.log("404 - Lapa nav atrasta");
+    }
+    if (code == 502){
+        console.log("lapa ir atrasta");
+    }   else{
+        console.log("404 - Lapa nav atrasta");
+    }
+}
